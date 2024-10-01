0bde54114b25d0ad8cebed0dd5bad46d9448d5d0
Nicolás Aranguren Turmequé <nicolasarangurenturmeque@Laptop-de-Nicolas.local>
2024-10-01 14:18:33 -0500

    Upload file bug.

--- api/routers/users.py
+++ api/routers/users.py
@@ -18,7 +18,8 @@ router = APIRouter(
 # Load batch data (max 3000 rows).
 @router.post("/load_data")
 def load_users(cols_to_hash:str, parent_path:str, file:UploadFile=Annotated[bytes, File(...)]):
-    separator = utils.separator_finder(os.path.join(parent_path, file.filename))
+    separator = utils.separator_finder(file.file)
+    # separator = utils.separator_finder(os.path.join(parent_path, file.filename))
     
     data = pd.read_csv(file.file, sep=separator, encoding="utf-8")
     
